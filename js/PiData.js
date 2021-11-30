7e934f5de4eb916038d3013847cf4a63e0ca06f3
Aleksander Kruse Schaumburg-Müller <alek1082@edu.easj.dk>
2021-11-30 12:55:03 +0100

    AleksEditIndexRenamed

--- js/PiData.js
+++ js/PiData.js
@@ -1,15 +1,10 @@
 const baseUrl = "https://temperaturerestget.azurewebsites.net/Sensor"
 
-const WeatherUrl = "https://www.7timer.info/bin/civillight.php?lon=12.1&lat=55.6&ac=0&unit=metric&output=json&tzshift=0"
-
-
 Vue.createApp({
     data() {
         return {
             temperatures: [],
-            dataseries: [],
 
-            
 
         }
     },
@@ -29,24 +24,12 @@ Vue.createApp({
             }
         },
 
-        async helperGetAndShowWeather(WeatherUrl) { 
-            try {
-                const response = await axios.get(WeatherUrl)
-                this.dataseries = await response.data
-            } catch (ex) {
-                alert(ex.message) 
-            }
-        },
-
-        getAllWeathers() {
-            this.helperGetAndShow(WeatherUrl)
-        },
-    
-        async sortTemperature() {
-            const url = baseUrl + "?sort_by=temperature"
+          
+        async sortByTemperature() {
+            const url = baseUrl + "?sort_by=Temperature"
             try {
                 const response = await axios.get(url)
-                this.temperatures = await response.data
+                this.temperatures.sort() = await response.data
             } catch (ex) {
                 alert(ex.message)
             }
