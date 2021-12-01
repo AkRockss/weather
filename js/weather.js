82ba3c958d092ec435395adc7cde2a532160b650
Frugten <75611171+Frugten@users.noreply.github.com>
2021-12-01 10:58:18 +0100

    Month now

--- js/weather.js
+++ js/weather.js
@@ -39,6 +39,8 @@
         const temptmaxElement6 = document.querySelector(".weather-temptmax6");
         const temptminElement6 = document.querySelector(".weather-temptmin6");
     $.getJSON('https://www.7timer.info/bin/civillight.php?lon=12.1&lat=55.6&ac=0&unit=metric&output=json&tzshift=0', function(data) {
+        var months = ["notzero", "Jan", "Feb", "Mar", "Apr", "Maj", "Jun", 
+           "Jul", "Aug", "Sep", "Okt", "Nov", "Dec" ];
     
     
         var day = `${data.dataseries[0].date}`
@@ -49,7 +51,9 @@
         const splittet = day.substring(6, );
         const splittet1 = day.substring(4,6);
 
-        dayElement.innerHTML = `${splittet}/${splittet1}`;
+        var selectedMonthName = months[splittet1];
+
+        dayElement.innerHTML = `${splittet} ${selectedMonthName}`;
         iconElement.innerHTML = `<img src="images/icons/${iconId}.svg" alt="" width=100/>`;
         temptmaxElement.innerHTML = `${temptmax}°C`;
         temptminElement.innerHTML = `${temptmin}°C`;
@@ -63,7 +67,9 @@
         const day1splittet = day1.substring(6, );
         const day1splittet1 = day1.substring(4,6);
 
-        day1Element1.innerHTML = `${day1splittet}/${day1splittet1}`;
+        var selectedMonthName1 = months[day1splittet1];
+
+        day1Element1.innerHTML = `${day1splittet} ${selectedMonthName1}`;
         iconElement1.innerHTML = `<img src="images/icons/${iconId1}.svg" alt="" width=48>`;
         temptmaxElement1.innerHTML = `${temptmax1}°C`;
         temptminElement1.innerHTML = `${temptmin1}°C`;
@@ -76,8 +82,10 @@
 
         const day2splittet = day2.substring(6, );
         const day2splittet1 = day2.substring(4,6);
+
+        var selectedMonthName2 = months[day2splittet1];
         
-        day2Element2.innerHTML = `${day2splittet}/${day2splittet1}`;
+        day2Element2.innerHTML = `${day2splittet} ${selectedMonthName2}`;
         iconElement2.innerHTML = `<img src="images/icons/${iconId2}.svg" alt="" width=48/>`;
         temptmaxElement2.innerHTML = `${temptmax2}°C`;
         temptminElement2.innerHTML = `${temptmin2}°C`;
@@ -90,7 +98,9 @@
         const day3splittet = day3.substring(6, );
         const day3splittet1 = day3.substring(4,6);
 
-        day3Element3.innerHTML = `${day3splittet}/${day3splittet1}`;
+        var selectedMonthName3 = months[day3splittet1];
+
+        day3Element3.innerHTML = `${day3splittet} ${selectedMonthName3}`;
         iconElement3.innerHTML = `<img src="images/icons/${iconId3}.svg" alt="" width=48/>`;
         temptmaxElement3.innerHTML = `${temptmax3}°C`;
         temptminElement3.innerHTML = `${temptmin3}°C`;
@@ -104,7 +114,9 @@
         const day4splittet = day4.substring(6, );
         const day4splittet1 = day4.substring(4,6);
 
-        day4Element4.innerHTML = `${day4splittet}/${day4splittet1}`;
+        var selectedMonthName4 = months[day4splittet1];
+
+        day4Element4.innerHTML = `${day4splittet} ${selectedMonthName4}`;
         iconElement4.innerHTML = `<img src="images/icons/${iconId4}.svg" alt="" width=48/>`;
         temptmaxElement4.innerHTML = `${temptmax4}°C`;
         temptminElement4.innerHTML = `${temptmin4}°C`;
@@ -118,7 +130,9 @@
         const day5splittet = day5.substring(6, );
         const day5splittet1 = day5.substring(4,6);
 
-        day5Element5.innerHTML = `${day5splittet}/${day5splittet1}`;
+        var selectedMonthName5 = months[day5splittet1];
+
+        day5Element5.innerHTML = `${day5splittet} ${selectedMonthName5}`;
         iconElement5.innerHTML = `<img src="images/icons/${iconId5}.svg" alt="" width=48/>`;
         temptmaxElement5.innerHTML = `${temptmax5}°C`;
         temptminElement5.innerHTML = `${temptmin5}°C`;
@@ -132,7 +146,9 @@
         const day6splittet = day6.substring(6, );
         const day6splittet1 = day6.substring(4,6);
 
-        day6Element6.innerHTML = `${day6splittet}/${day6splittet1}`;
+        var selectedMonthName6 = months[day6splittet1];
+
+        day6Element6.innerHTML = `${day6splittet} ${selectedMonthName6}`;
         iconElement6.innerHTML = `<img src="images/icons/${iconId6}.svg" alt="" width=48/>`;
         temptmaxElement6.innerHTML = `${temptmax6}°C`;
         temptminElement6.innerHTML = `${temptmin6}°C`;
