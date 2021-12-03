d9efc07aa44fe25fca74004bbea687baca8fa267
Nicolas Lauridsen <nicolas.lauridsen@gmail.com>
2021-12-03 10:17:35 +0100

    Average for 5 days

--- js/PiData.js
+++ js/PiData.js
@@ -1,9 +1,11 @@
 const baseUrl = "https://temperaturerestget.azurewebsites.net/Sensor"
+const baseUrl2 = "https://temperaturerestget.azurewebsites.net/GetAverage"
 
 Vue.createApp({
     data() {
         return {
             temperatures: [],
+            temperaturesAverage: [],
             averageTemps: 0,
             averageHumid: 0,
 
@@ -14,21 +16,41 @@ Vue.createApp({
 
         getAllTemperatures() {
             this.helperGetAndShow(baseUrl)
-            
+
         },
-    
-        async helperGetAndShow(url) { 
+
+        getAverage144() {
+            this.helperGetAndShowAvg(baseUrl2)
+
+        },
+
+        async helperGetAndShow(url) {
             try {
                 const response = await axios.get(url)
                 this.temperatures = await response.data
-                this.averageTemps = await (this.temperatures[0].temperature + this.temperatures[1].temperature + this.temperatures[2].temperature + this.temperatures[3].temperature + this.temperatures[4].temperature) / 5
-                this.averageHumid = await (parseFloat(this.temperatures[0].humidity) + parseFloat(this.temperatures[1].humidity) + parseFloat(this.temperatures[2].humidity) + parseFloat(this.temperatures[3].humidity) + parseFloat(this.temperatures[4].humidity)) / 5
+
             } catch (ex) {
-                alert(ex.message) 
+                alert(ex.message)
             }
         },
-    }
 
+        async helperGetAndShowAvg(url) {
+            try {
+                const response = await axios.get(url)
+                this.temperaturesAverage = await response.data
+
+                for await (temp of await this.temperaturesAverage) {
+                    this.averageTemps += await temp.temperature
+                    this.averageHumid += await parseFloat(temp.humidity)
+                }
+                this.averageTemps = await this.averageTemps / this.temperaturesAverage.length
+                this.averageHumid = await this.averageHumid / this.temperaturesAverage.length
+
+            } catch (ex) {
+                alert(ex.message)
+            }
+        }
+    }
 }).mount("#app")
 
 
